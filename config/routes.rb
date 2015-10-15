1ecce7e95845b1a0a4ae814f0f4c81a34197d853
Dave Carlson <davicar319@acm.org>
2015-10-14 20:39:15 -0500

    Moved business logic into the controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'greeter/hello'
+  get 'greeter/goodbye'
+  get 'greeter/hi' => 'greeter#hello'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
